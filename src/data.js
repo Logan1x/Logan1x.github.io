3dd343cf3be2f110882440d6ad19b663382d58c9
Khushal Sharma <sharmakhushal78@gmail.com>
2024-06-30 21:09:12 +0530

    Update new projects

--- src/data.js
+++ src/data.js
@@ -1,5 +1,22 @@
 export const projectData = [
   {
+    projectName: "iQuiz",
+    projectDate: "Jun 2024",
+    projectDescription:
+      "iQuiz is a fullstack AI-based quiz application that leverages OpenAI to generate quizzes on various topics.",
+    tags: ["ReactJS", "Nextjs", "Typescript", "PostgreSQL", "OpenAI"],
+    link: "https://iquiz-ai.vercel.app/",
+    repo: "https://github.com/Logan1x/iQuiz",
+  },
+  {
+    projectName: "SRTify",
+    projectDate: "Feb 2024",
+    projectDescription:
+      "AI-powered platform that produces subtitles in multiple languages directly from your videos.",
+    tags: ["ReactJS", "Nextjs", "Typescript", "PostgreSQL", "Python", "FastAPI", "Whisper"],
+    repo: "https://github.com/Logan1x/SRTify",
+  },
+  {
     projectName: "Check In Mate",
     projectDate: "Jan 2024",
     projectDescription:
